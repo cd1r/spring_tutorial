45f305f064282edf2f49254c4d03f7480a9403e6
SeyoonMsi <cd1r@na>
2017-02-22 19:43:47 +0900

    Commit at SelectoCoffee

--- src/main/java/org/zerock/controller/BoardController.java
+++ src/main/java/org/zerock/controller/BoardController.java
@@ -6,11 +6,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.zerock.domain.BoardVO;
+import org.zerock.domain.Criteria;
+import org.zerock.domain.PageMaker;
 import org.zerock.service.BoardService;
 
 @Controller
@@ -41,32 +44,56 @@ public class BoardController {
 	}
 	
 	@RequestMapping(value = "/listAll", method = RequestMethod.GET)
-	public void listAll(Model model) throws Exception{
+	public void listAll(Criteria cri, Model model) throws Exception{
 		logger.info("show all list...");
-		model.addAttribute("list", service.listAll());
+		//model.addAttribute("list", service.listAll());
+		model.addAttribute("list", service.listCriteria(cri));
 	}
 	
 	@RequestMapping(value = "/read", method = RequestMethod.GET)
-	public void read(@RequestParam("bno") int bno, Model model) throws Exception{
+	public void read(@RequestParam("bno") int bno,
+					@ModelAttribute("cri") Criteria cri,
+					Model model) throws Exception{
+		
 		model.addAttribute(service.read(bno));
 	}
 	
-	@RequestMapping(value = "/remove", method = RequestMethod.POST)
+	/*@RequestMapping(value = "/remove", method = RequestMethod.POST)
 	public String remove(@RequestParam("bno") int bno, RedirectAttributes rttr) throws Exception{
 		
 		service.remove(bno);
 		rttr.addFlashAttribute("msg", "success");
 		
 		return "redirect:/board/listAll";
+	}*/
+	
+	@RequestMapping(value="/removePage", method = RequestMethod.POST)
+	public String remove(@RequestParam("bno") int bno,
+			Criteria cri, RedirectAttributes rttr) throws Exception{
+		service.remove(bno);
+		
+		rttr.addAttribute("page", cri.getPage());
+		rttr.addAttribute("perPageNum", cri.getPerPageNum());
+		rttr.addFlashAttribute("msg", "success"); //임시로 사용하는 데이터인 경우에 사용
+		
+		return "redirect:/board/listPage";
 	}
 	
-	@RequestMapping(value = "/modify", method = RequestMethod.GET)
+	/*@RequestMapping(value = "/modify", method = RequestMethod.GET)
 	public void modifyGET(int bno, Model model) throws Exception{
+		
+		model.addAttribute(service.read(bno));
+	}*/
+	
+	@RequestMapping(value = "/modifyPage", method = RequestMethod.GET)
+	public void modifyGET(@RequestParam("bno")int bno,
+						@ModelAttribute("cri") Criteria cri,
+						Model model) throws Exception{
 		
 		model.addAttribute(service.read(bno));
 	}
 	
-	@RequestMapping(value="/modify", method = RequestMethod.POST)
+	/*@RequestMapping(value="/modify", method = RequestMethod.POST)
 	public String modifyPOST(BoardVO board, RedirectAttributes rttr) throws Exception{
 		logger.info("mod post......");
 		
@@ -74,5 +101,32 @@ public class BoardController {
 		rttr.addFlashAttribute("msg", "success");
 		
 		return "redirect:/board/listAll";
+	}*/
+	
+	@RequestMapping(value="/modifyPage", method = RequestMethod.POST)
+	public String modifyPagingPOST(BoardVO board, Criteria cri, 
+								RedirectAttributes rttr) throws Exception{
+				
+		service.modify(board);
+		
+		rttr.addAttribute("page", cri.getPage());
+		rttr.addAttribute("perPageNum", cri.getPerPageNum());
+		rttr.addFlashAttribute("msg", "success");
+		
+		return "redirect:/board/listPage";
+	}
+	
+	@RequestMapping(value="/listPage", method = RequestMethod.GET)
+	public void listPage(@ModelAttribute("cri")Criteria cri, Model model)throws Exception{
+		logger.info(cri.toString());
+		
+		model.addAttribute("list", service.listCriteria(cri));
+		PageMaker pageMaker = new PageMaker();
+		pageMaker.setCri(cri);
+		//pageMaker.setTotalCount(131);
+		
+		pageMaker.setTotalCount(service.listCountCriteria(cri));
+		
+		model.addAttribute("pageMaker", pageMaker);
 	}
 }
--- src/main/java/org/zerock/service/BoardService.java
+++ src/main/java/org/zerock/service/BoardService.java
@@ -3,6 +3,7 @@ package org.zerock.service;
 import java.util.List;
 
 import org.zerock.domain.BoardVO;
+import org.zerock.domain.Criteria;
 
 public interface BoardService {
 
@@ -11,4 +12,6 @@ public interface BoardService {
 	public void modify(BoardVO board) throws Exception;
 	public void remove(int bno) throws Exception;
 	public List<BoardVO> listAll() throws Exception;
+	public List<BoardVO> listCriteria(Criteria cri) throws Exception;
+	public int listCountCriteria(Criteria cri) throws Exception;
 }
